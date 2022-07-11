646d0ebb85e3fdd1e99584f8b12e78eee09e1c14
JosueQuevedo <josuedqp18@gmail.com>
2022-07-11 15:31:37 -0400

    arrSort

---
+++ day1/Index.js
@@ -0,0 +1,8 @@
+// Write a function that takes an array of numbers as argument
+// It should return an array with the numbers sorted in descending order
+
+const order = (arr) => {
+	return arr.sort();
+};
+
+console.log(order([8, 4, 6, 1, 2]));
