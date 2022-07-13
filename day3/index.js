1c80d2f4080cfd645c3edf3e859dc8846c99e82c
JosueQuevedo <josuedqp18@gmail.com>
2022-07-13 12:52:37 -0400

    this is the day 3

---
+++ day3/index.js
@@ -0,0 +1,7 @@
+// escriba una funcion que tome como un array como argumento (a) y un número (n) como argumento deberá devolver los ultimós (n) elementos del array.
+
+const lastElements = (a, n) => {
+	return a.slice(-n);
+};
+
+console.log(lastElements([1, 2, 3, 4, 5, 'num'], 3));
