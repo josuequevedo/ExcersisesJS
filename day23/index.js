c4b653cb3e4d457396bc1d5236f6d0f13167a19e
JosueQuevedo <josuedqp18@gmail.com>
2022-08-17 14:15:12 -0400

    day23

---
+++ day23/index.js
@@ -0,0 +1,7 @@
+//* elimine los espacios de la cadena y luego devuelva la cadena resultante.
+
+const replaceSpace = (str) => {
+	return str.replace(/\s/g, '');
+};
+
+console.log(replaceSpace('8 j 8   mBliB8g  imjB8B8  jl  B'));
